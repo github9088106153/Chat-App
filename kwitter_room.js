e5d21bc11b14f642eb2386144d9beb5a3909e1d2
github9088106153 <87702995+github9088106153@users.noreply.github.com>
2022-01-27 23:48:30 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -9,24 +9,44 @@
       measurementId: "G-3WCBQ9WV6V"
     };
     
-   firebase.initializeApp(firebaseConfig);
-
-function getData()
+    firebase.initializeApp(firebaseConfig);
+    user_name=localStorage.getItem("user_name");
+ document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+ 
+ function add_room()
  {
-       firebase.database().ref("/").on('value', function(snapshot) 
-       {
-             document.getElementById("output").innerHTML = "";
-             snapshot.forEach(function(childSnapshot) 
-             {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });
+   room_name = document.getElementById("room_name").value;
+ 
+   firebase.database().ref("/").child(room_name).update({
+     purpose : "adding room name"
+   });
+ 
+     localStorage.setItem("room_name", room_name);
+     
+     window.location = "kwitter_page.html";
+ }
+ 
+ function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+        Room_names = childKey;
+        console.log("Room Name - " + Room_names);
+       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+       document.getElementById("output").innerHTML += row;
      });
-      }
-getData();
-function logout()
-{
-      window.location="index.html";
-}
+   });
+ 
+ }
+ 
+ getData();
+ 
+ function redirectToRoomName(name)
+ {
+   console.log(name);
+   localStorage.setItem("room_name", name);
+     window.location = "kwitter_page.html";
+ }
+ 
+ function logout() {
+ localStorage.removeItem("user_name");
+ localStorage.removeItem("room_name");
+     window.location = "index.html";}
+ 
